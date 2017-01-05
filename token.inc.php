a097728e7c9adc8e49af20de92b6e6d3d017bd53
candra1104 <candra.setyawan@gmail.com>
2017-01-05 22:35:12 +0700

    folder pertama

---
+++ token.inc.php
@@ -0,0 +1,7 @@
+<?php
+//teletransaksi
+$idtoken = '320346100';
+$guidtoken = 'AAEKdNCBAHRS0UpCa2sx2tSKBc5VqTv2UqY';
+$namatoken = '@TeleTransAksibot';
+//btronik 245583532:AAE5oR_LtAP6zsRHx6Z3HtHXmKjXZyu7evc
+?>
