cb1534958a0faf4fc83b31572a0e9d2917fa4078
candra1104 <yandra_co@yahoo.com>
2017-01-13 08:29:23 +0700

    ganti token disini

--- token.inc.php
+++ token.inc.php
@@ -1,7 +1,7 @@
 <?php
-//teletransaksi
-$idtoken = '320346100';
-$guidtoken = 'AAEKdNCBAHRS0UpCa2sx2tSKBc5VqTv2UqY';
-$namatoken = '@TeleTransAksibot';
-//btronik 245583532:AAE5oR_LtAP6zsRHx6Z3HtHXmKjXZyu7evc
+// set di sini
+$idtoken = 'idtokennya';
+$guidtoken = 'guidtokennya';
+$namatoken = '@namatokennya';
+// end set
 ?>
